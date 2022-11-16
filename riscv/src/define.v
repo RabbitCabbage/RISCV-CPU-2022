ec654b440cb6185cb2338703135deb172800dbf3
shen-dong <shen-dong@sjtu.edu.cn>
2022-11-16 13:11:41 +0800

    init

---
+++ riscv/src/define.v
@@ -0,0 +1,3 @@
+`define ICSIZE 512 //ICache的大小
+`define INSTRLEN 5:0 //Instruction的长度
+`define ADDR 31:0
---
+++ riscv/src/icache.v
@@ -0,0 +1,20 @@
+`include "define.v"
+
+//从mem中预取指令，然后按照pc把指令取给IF
+module ICache (
+  //control signals
+  input wire clk,
+  input wire rst,
+  input wire rdy,
+
+  //IF module
+  //IF requires, and ICache gives
+  input wire [`ADDR] require_addr,
+  output wire[`INSTRLEN] if_instr,
+
+  //RAM module 
+  //ICache requires, and RAM gives
+  input wire [`INSTRLEN] mem_instr,
+  output wire[`ADDR] mem_addr
+);
+endmodule
