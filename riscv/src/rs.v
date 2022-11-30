2f18d41782c96ff89154d8f9c1b110f26f979772
shen-dong <shen-dong@sjtu.edu.cn>
2022-11-30 20:02:40 +0800

    Reservation Station

--- riscv/src/rs.v
+++ riscv/src/rs.v
@@ -8,17 +8,32 @@ module RS(
     
     //from decoder
     input wire decode_success,
-    input wire [`INSTRLEN] decode_instr,
-    input wire [`REGINDEX] decode_rs1,
-    input wire [`REGINDEX] decode_rs2
+    input wire [`ROBINDEX] decode_rs1_rename,
+    input wire [`ROBINDEX] decode_rs2_rename,
+    input wire [`DATALEN] decode_rs1_value,
+    input wire [`DATALEN] decode_rs2_value,
     input wire [`IMMLEN] decode_imm,
-    input wire [`REGINDEX] decode_rd,
+    input wire [`ROBINDEX] decode_rd_rename,
     input wire [`OPLEN] decode_op,
     input wire [`ADDR] decpde_pc,
 
-    // to ALU
+    //monitor the updated renaming from the cbd
+    //input the number and renaming from alu
+    input wire alu_broadcast,
+    input wire [`DATALEN] alu_cbd_value,
+    input wire [`ROBINDEX] alu_update_rename,
+    //like add/sub these will directly get answer from the alu
+    input wire lsb_broadcast,
+    input wire [`DATALEN] lsb_cbd_value,
+    input wire [`ROBINDEX] lsb_updated_rename,
+    //load get answer from the lsb, load data to the reg
+    input wire rob_broadcast,
+    input wire [`DATALEN] rob_cbd_value,
+    input wire [`ROBINDEX] rob_update_rename,
+    //get answer from the rob, such as put the answer from the rob
+
     output wire alu_enable,
-    output wire to_alu_renaming,
+    output wire [`ROBINDEX] to_alu_rd_renaming,
     output wire [`REGLINE] to_alu_rs1_value,
     output wire [`REGLINE] to_alu_rs2_value,
     output wire [`OPLEN] to_alu_op,
@@ -26,27 +41,14 @@ module RS(
     output wire [`ADDR] to_alu_pc,
 
     output wire rs_full,//如果rs满了的话就要停下if
-    
-    // from ROB and LSB
-    // record the index of this instr in ROB
-    // ROB要把写好到reg中的东西返回到RS让它去除renaming
-    input wire rob_is_update,
-    input wire updated_reg_index,
-    //to ROB module    
-    output wire [`REGINDEX] rd,
-    //to LSB module
-    output wire [`ADD] ls_information,
-    output wire [`INSTRLEN] instr
 )
 reg [`OPLEN] opcode[`RSSIZE];
 reg [`DATALEN] rs1_value[`RSSIZE];
 reg [`DATALEN] rs2_value[`RSSIZE];
-reg [`ROBINDEX] rs1_rename[`RSSIZE];
+reg [`ROBINDEX] rs1_rename[`RSSIZE];//如果rename成了ROBNOTRENAME就表示已经ready了可以进行计算了
 reg [`ROBINDEX] rs2_rename[`RSSIZE];
 reg [`ROBINDEX] rd_rename[`RSSIZE];
 reg [`IMMLEN] imm[`RSSIZE];
-reg rs1_rdy[`RSSIZE];
-reg rs2_rdy[`RSSIZE];
 reg ready[`RSSIZE];
 reg busy[`RSSIZE];
 reg [`ADDR] pc[`RSSIZE];
@@ -92,6 +94,7 @@ always @(*) begin
         alu_enable <=  `FALSE;
         for(i=0;i<32;i=i+1) begin
             busy[i] <= `FALSE;
+            opcode[i] <= 6'0;
         end
     end else if (rdy) begin
         // 如果在RS中又ready的index；
@@ -104,20 +107,66 @@ always @(*) begin
             to_alu_imm <= imm[issue_index];
             to_alu_renaming <= rd_rename[issue_index];
             to_alu_pc <= pc[issue_index];
-            busy[issue_index] <= `FALSE;
+            busy[issue_index] <= `FALSE;//这条rs就可以使用了
         end
         //如果decode这边成功解码，并且有空位置，添加一条指令
         if(decode_success && free_index!=`RSNOTFOUND) begin
             busy[free_index] <= `TRUE;
-            tags[free_index] <= in_decode_rob_tag;
+            tags[free_index] <= decode_rd_rename;
             opcode[free_index] <= decode_op;
-            imm[free_index] <= decode_imml;
+            imm[free_index] <= decode_imm;
             pc[free_index] <= decode_pc;
             rs1_value[free_index] <= decode_rs1_value;
             rs2_value[free_index] <= decode_rs2_value;
             rs1_rename[free_index] <= decode_rs1_rename;
             rs2_rename[free_index] <= decode_rs2_rename;
         end     
+        //monitor alu lsb and rob
+        if(alu_broadcast==`TRUE) begin
+            for(i=0;i<16;i=i+1) begin
+                if(busy[i]==`TRUE) begin
+                    if(rs1_rename[i]==alu_update_rename) begin
+                        //说明这个要被替换掉了
+                        rs1_rename[i] <= `ROBNOTRENAME;
+                        rs1_value[i] <= alu_cbd_value;
+                    end else if(rs2_rename[i]==alu_update_rename) begin
+                        rs2_rename[i] <= `ROBNOTRENAME;
+                        rs2_value[i] <= alu_cbd_value;
+                    end
+                end
+            end
+        end
+        if(lsb_broadcast==`TRUE) begin
+            for(i=0;i<16;i=i+1) begin
+                if(busy[i]==`TRUE) begin
+                    if(rs1_rename[i]==lsb_updated_rename) begin
+                        rs1_rename[i] <= `ROBNOTRENAME;
+                        rs1_value[i] = lsb_cbd_value;
+                    end else if(rs2_rename[i]==lsb_update_rename) begin
+                        rs2_rename[i] <= `ROBNOTRENAME;
+                        rs2_value[i] <= lsb_cbd_value;
+                    end
+                end
+            end
+        end
+        if(rob_broadcast==`TRUE) begin
+            for(i=0;i<16;i=i+1) begin
+                    if(busy[i]) begin
+                    if(rs1_rename[i]==rob_update_rename) begin
+                        rs1_rename[i] <= `ROBNOTRENAME;
+                        rs1_value[i] <= rob_cbd_value;
+                    end else if(rs2_rename[i]==rob_update_rename) begin
+                        rs2_rename[i] <= `ROBNOTRENAME;
+                        rs2_value[i] <= rob_cbd_value;
+                    end
+                end
+            end
+        end
+        for(i=0;i<16;i=i+1) begin
+            if(rs1_rename[i]==`ROBNOTRENAME && rs2_rename[i]==`ROBNOTRENAME) begin
+                ready[i] <= `TRUE;
+            end
+        end
     end
 end
 endmodule
