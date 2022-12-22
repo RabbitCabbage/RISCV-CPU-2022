a8b79fd91e176478336ac70269be30989294f480
shen-dong <shen-dong@sjtu.edu.cn>
2022-12-22 14:57:33 +0800

    Debug Version 3

--- riscv/test/test.c
+++ riscv/test/test.c
@@ -1,6 +1,25 @@
-int main(){
-    int a = 5;
-    int b = a + 1;
-    int c = a + 2;
-    return 0;
+#include "io.h"
+//input: 1 2 3 4
+
+int a[4];
+int main()
+{
+    int b[4];
+	int i;
+    for (i = 0; i < 4; i++)
+	{
+		a[i] = 0;
+		b[i] = i + 1;
+	}
+	for (i = 0; i < 4; i++)
+	{
+		outl(a[i]);
+	}
+	println("");
+	int *p;
+	p=b;
+	for (i = 0; i < 4; i++)
+	{
+		outl(p[i]);
+	}
 }
